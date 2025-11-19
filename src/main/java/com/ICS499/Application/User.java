5f883a019e7f55cfb3b8383c032c7fdf4d2aee3f
Xuan Phung <thanhxuanlop9.10@gmail.com>
2025-11-19 17:29:06 -0600

    register user,reset and forgot password process, user control

--- src/main/java/com/ICS499/Application/User.java
+++ src/main/java/com/ICS499/Application/User.java
@@ -33,6 +33,12 @@ public class User {
     @Column(name = "password")
     private String password;
 
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
     public Long getId() {return id;}
 
     public void setId(Long id) {this.id = id;}
@@ -44,6 +50,12 @@ public class User {
     public String getPassword() {return password;}
 
     public void setPassword(String password) {this.password = password; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
     @Override
     public final boolean equals(Object o) {
--- src/main/java/com/ICS499/Application/repositories/UserRepository.java
+++ src/main/java/com/ICS499/Application/repositories/UserRepository.java
@@ -3,8 +3,12 @@ package com.ICS499.Application.repositories;
 import com.ICS499.Application.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
     boolean existsByEmail(String email);
+
+    List<User> email(String email);
 }
